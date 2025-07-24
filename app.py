18fd20d164b2b5136f5eb78673c8720ce29a8e78
Alex Wong <likhang.wong97@gmail.com>
2025-07-23 20:00:00 -0700

    edited app.py for analytics page

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ import streamlit as st
 st.set_page_config(layout="wide")
 
 sales_monitor_page = st.Page("streamlit_pages/sales_monitor.py", title="Sales monitor", icon=":material/finance_mode:")
+analytics_page = st.Page("streamlit_pages/analytics.py", title="Sales analytics", icon=":material/finance_mode:")
 diagnostics_total_page = st.Page("streamlit_pages/diagnostics_total_sales.py", title="Model diagnostics - Total Sales", icon=":material/monitor_heart:")
 diagnostics_item_A_page = st.Page("streamlit_pages/diagnostics_item_A.py", title="Model diagnostics - Item A Sales", icon=":material/monitor_heart:")
 diagnostics_item_B_page = st.Page("streamlit_pages/diagnostics_item_B.py", title="Model diagnostics - Item B Sales", icon=":material/monitor_heart:")
 diagnostics_orders_page = st.Page("streamlit_pages/diagnostics_orders.py", title="Model diagnostics - In Store Orders", icon=":material/monitor_heart:")
 
-pg = st.navigation({"Page selection":[sales_monitor_page, diagnostics_total_page, diagnostics_item_A_page, diagnostics_item_B_page,
+pg = st.navigation({"Welcome to SweetPulse!":[sales_monitor_page, analytics_page, diagnostics_total_page, diagnostics_item_A_page, diagnostics_item_B_page,
                                       diagnostics_orders_page]})
 pg.run()
